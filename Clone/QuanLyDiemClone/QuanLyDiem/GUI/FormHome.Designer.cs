90ec04a38cd7f209c08852c363d15b21727bc1eb
A_tan <taubeopro@gmail.com>
2019-01-09 19:39:35 +0700

    fix lbale

--- Clone/QuanLyDiemClone/QuanLyDiem/GUI/FormHome.Designer.cs
+++ Clone/QuanLyDiemClone/QuanLyDiem/GUI/FormHome.Designer.cs
@@ -108,7 +108,7 @@
             // labelUserName
             // 
             this.labelUserName.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.labelUserName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelUserName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelUserName.ForeColor = System.Drawing.Color.Blue;
             this.labelUserName.Location = new System.Drawing.Point(0, 0);
             this.labelUserName.Name = "labelUserName";
